f797440031dde45f03fd0b9405f5f46a84afe7e4
florian <vlo@guez.li>
2012-04-10 13:21:27 +0200

    Seeds corrected (no inline HTML)

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,25 +10,25 @@
 Product.delete_all
 
 Product.create(title: 'Wer das liest ist doof',
-  description: %{<p>
+  description: %{
       Isabelle Chevalley (glp., Waadt) forderte im Rahmen einer Änderung des Tierschutzgesetzes, 
       dass das Halten von Wildtieren, die besondere Ansprüche an Haltung und Pflege stellten, 
       einer Bewilligung bedürfe. Artikel 7 des Tierschutzgesetzes sollte zudem wie folgt ergänzt 
       werden: Die Delphin- und Walhaltung (Cetacea) ist verboten. Der Nationalrat stimmte dem 
-      Einzelantrag überraschend mit 112 zu 60 Stimmen zu.</p>
+      Einzelantrag überraschend mit 112 zu 60 Stimmen zu.
   },
   image_url: 'ruby.jpg',
   price: 49.34)
 
 Product.create(title: 'Wer das liest ist noch doofer',
-  description: %{<p>
+  description: %{
       Im November vergangenen Jahrs waren im Connyland innerhalb einer Woche die beiden Delphine
       Shadow und Chelmers verendet (siehe Box). Tierschutzorganisationen hatten die Delphinhaltung
       im Connyland nicht nur in Zusammenhang mit dem Tod der Delphine heftig kritisiert. Die
       fr&uuml;here Nationalrätin Brigitta Gadient (bdp., Graub&uuml;nden) forderte in einer Motion ein
       Importverbot für Delphine. Der Bundesrat lehnt ein solches Verbot ab. Er teilte im Dezember
       2011 aber mit, er wolle die Untersuchung zum Tod der zwei Delphine im thurgauischen Lipperswil
-      abwarten und prüfen, ob die Regeln bei der Haltung verschärft werden sollten.</p>
+      abwarten und prüfen, ob die Regeln bei der Haltung verschärft werden sollten.
   },
   image_url: 'rubyx.jpg',
   price: 1493.00)
