06a34f0de8dc63cfe1e46d49cd519a192ca4f562
florian <vlo@guez.li>
2012-05-24 20:20:22 +0200

    sdfsdf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   private
-  
+
     def current_cart
-       Cart.find(session[:cart_id])
-    rescue
-       ActiveRecord::RecordNotFound
-       brand_new_cart = Cart.create
-       session[:cart_id] = brand_new_cart.id
-       brand_new_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
     end
-  
 end
