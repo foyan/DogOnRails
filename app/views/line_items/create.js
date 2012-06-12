83959e8bd0efbe3de0b225f84a8e264598ea4937
florian <vlo@guez.li>
2012-06-12 15:39:08 +0200

    Animalic animation

--- app/views/line_items/create.js
+++ app/views/line_items/create.js
@@ -1,1 +1,5 @@
-document.getElementById('cart').innerHTML = '<%= escape_javascript(render(@cart)) %>';
+$('#cart').html('<%= escape_javascript(render(@cart)) %>');
+
+$('#current_item').css({'background-color':'blue', 'color':'white'}).
+  animate({'background-color':'white', 'color':'black'}, 1000);
+  
